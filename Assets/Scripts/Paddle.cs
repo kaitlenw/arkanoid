5e8e832624ccf0b6ff7b6119227b9fd4f88fe73e
Kaitlen <kaitlenw@outlook.com>
2020-03-26 14:45:37 +0000

    Added basic mechanisms for arkanoid game

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    public float speed;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        rb.MovePosition(transform.position + new Vector3(x*speed, 0, 0));
+    }
+}
