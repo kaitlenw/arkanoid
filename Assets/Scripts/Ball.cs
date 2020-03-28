8f557e6032580717e28e5aa12b9a5f653b20ba97
Kaitlen <kaitlenw@outlook.com>
2020-03-28 16:04:19 +0000

    Added Game Over

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,22 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
 
     public float speed;
+    public int startLives;
+    private int livesLeft;
     private Rigidbody2D rb;
     private bool gameHasStarted = false;
-    // Start is called before the first frame update
+    private Vector2 startPos;
+    public Sprite sprite; 
+    public GameObject lifeDisplay;
+    public GameObject winPanel;
+    public GameObject gameOverPanel;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        startPos = transform.position;
+        livesLeft = startLives;
+        winPanel.SetActive(false);
+        gameOverPanel.SetActive(false);
     }
 
     void Update()
     {
-        if ((!gameHasStarted) && Input.GetButton("Jump"))
+        if ((!gameHasStarted) && Input.GetButton("Jump") && (livesLeft >= 0))
         {
             rb.velocity = (Vector2.up + new Vector2(Random.Range(-0.50f, 0.5f), 0)) * speed;
             gameHasStarted = true;
@@ -27,13 +38,24 @@ public class Ball : MonoBehaviour
         if (col.gameObject.tag == "Paddle") 
         {
             // Calculate hit Factor
-            float x=hitFactor(transform.position,col.transform.position, col.collider.bounds.size.x);
+            float x = hitFactor(transform.position,col.transform.position, col.collider.bounds.size.x);
 
             // Calculate direction, set length to 1
             Vector2 dir = new Vector2(x, 1).normalized;
 
             // Set Velocity with dir * speed
             rb.velocity = dir * speed;
+        }
+        else if (col.gameObject.tag == "Floor")
+        {
+            gameHasStarted = false;
+            rb.velocity = Vector2.zero;
+            transform.position = startPos;
+            livesLeft--;
+            if (livesLeft < 0)
+            {
+                gameOverPanel.SetActive(true);
+            }
         }
         else 
         {
@@ -52,5 +74,28 @@ public class Ball : MonoBehaviour
         // ===================  <- racket
         //
         return (ballPos.x - racketPos.x) / racketWidth;
+    }
+
+    void OnGUI()
+    {
+        foreach (Transform child in lifeDisplay.transform)
+        {
+            Destroy(child.gameObject);
+        }
+        for (int x = 0; x < startLives; x++)
+        {
+            GameObject life = new GameObject();
+            Image img = life.AddComponent<Image>();
+            img.preserveAspect = true;
+            img.sprite = sprite;
+            RectTransform rt = life.GetComponent<RectTransform>();
+            rt.sizeDelta = new Vector2(70, 70);
+            // draw image in box
+            if (x >= livesLeft)
+            {
+                img.enabled = false;
+            }
+            rt.SetParent(lifeDisplay.transform);
+        }
     }
 }
